fb76519e5fa8d624c33e4f0e49415b61a0526440
重田 将人 <masato.0928.j@gmail.com>
2020-07-28 22:08:06 +0900

    投稿機能の雛形だけ作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,23 @@
-class UsersController < ApplicationController
+class ArticlesController < ApplicationController
     def create
         @article = current_user.articles.new(
-            title: params[:title],
-            content: params[:content]
+            title: params[:article][:title],
+            content: params[:article][:content]
         )
 
-        @article.save
-
+        if @article.save
+            redirect_to article_path(id: @article.id)
+        else
+            render 'articles/new'
+        end
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def show
+        @article = Article.find_by(id: params[:id])
+    end
+
 end
