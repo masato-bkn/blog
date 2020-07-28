33d3a2abdc1fa4fb72a39427a895f6f71d8cb5cc
重田 将人 <masato.0928.j@gmail.com>
2020-07-29 07:31:03 +0900

    Articlesのcontentをtextに変更

---
+++ db/migrate/20200728222746_change_datatype_content_of_articles.rb
@@ -0,0 +1,5 @@
+class ChangeDatatypeContentOfArticles < ActiveRecord::Migration[6.0]
+  def change
+    change_column :articles, :content, 'text'    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_232229) do
+ActiveRecord::Schema.define(version: 2020_07_28_222746) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", default: "", null: false
-    t.string "content", default: "", null: false
+    t.text "content", default: "", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
