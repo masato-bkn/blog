cdd93a92f3176d913d6afa6ea667c5d217711fdd
重田 将人 <masato.0928.j@gmail.com>
2020-08-06 09:11:49 +0900

    rubocop

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,6 +1,6 @@
 class Good < ApplicationRecord
-    belongs_to :user
-    belongs_tp :article
-    validates :user_id, presence: true
-    validates :article_id, presence: true
+  belongs_to :user
+  belongs_tp :article
+  validates :user_id, presence: true
+  validates :article_id, presence: true
 end
