771d6838afac0a97b89f68b87ae088f24d14a512
重田 将人 <masato.0928.j@gmail.com>
2020-08-08 08:00:09 +0900

    rubocop

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,15 +1,14 @@
 class GoodsController < ApplicationController
-    def create
-        @article = current_user.articles.find_by(id: params[:article_id])
+  def create
+    @article = current_user.articles.find_by(id: params[:article_id])
 
-        if @article.present?
-            Good.create!(user_id: params[:user_id], article_id: params[:article_id])
-            render 'articles/show'
-        else
-            redirect_to articles_path
-        end
+    if @article.present?
+      Good.create!(user_id: params[:user_id], article_id: params[:article_id])
+      render 'articles/show'
+    else
+      redirect_to articles_path
     end
+  end
 
-    def destroy
-    end
+  def destroy; end
 end
--- spec/requests/goods_spec.rb
+++ spec/requests/goods_spec.rb
@@ -1,51 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe GoodsController, type: :request do
-    describe 'POST /goods' do
-        subject do
-          post goods_path(params)
-        end
-
-        let :params do
-            {
-                user_id: user_id,
-                article_id: article_id
-            }
-        end
-
-        let :user_id do
-            1
-        end
-
-        let :article_id do
-            1
-        end
-
-        let :user1 do
-            create(:user1)
-        end
-
-        let :article1 do
-            create(:article1)
-        end
-
-        context '記事が存在する場合' do
-            it "いいねできていること" do
-                expect do
-                    sign_in user1
-                    article1
-                    subject
-                end.to change(Good, :count).by(1)
-            end
-        end
-
-        context '記事が存在しない場合' do
-            it "いいねできないこと" do
-                expect do
-                    sign_in user1
-                    subject
-                end.to change(Good, :count).by(0)
-            end
-        end
+  describe 'POST /goods' do
+    subject do
+      post goods_path(params)
     end
+
+    let :params do
+      {
+        user_id: user_id,
+        article_id: article_id
+      }
+    end
+
+    let :user_id do
+      1
+    end
+
+    let :article_id do
+      1
+    end
+
+    let :user1 do
+      create(:user1)
+    end
+
+    let :article1 do
+      create(:article1)
+    end
+
+    context '記事が存在する場合' do
+      it 'いいねできていること' do
+        expect do
+          sign_in user1
+          article1
+          subject
+        end.to change(Good, :count).by(1)
+      end
+    end
+
+    context '記事が存在しない場合' do
+      it 'いいねできないこと' do
+        expect do
+          sign_in user1
+          subject
+        end.to change(Good, :count).by(0)
+      end
+    end
+  end
 end
