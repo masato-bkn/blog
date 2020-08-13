28abe6a2f60b5250337831fedb1b68104c3def4c
重田 将人 <masato.0928.j@gmail.com>
2020-08-13 09:36:55 +0900

    commentについてのテストを修正

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     id { 1 }
     text { 'test' }
     article_id { 1 }
+    user_id { 1 }
     updated_at { '2020-07-27 22:52:09' }
     created_at { '2020-07-27 22:52:09' }
   end
