e2119c79dfb3d8e5f8cb7b1233d6fc22734c0a57
重田 将人 <masato.0928.j@gmail.com>
2020-08-13 09:23:54 +0900

    user_idをcommentsテーブルに追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  # belongs_to :users
   default_scope -> { order(created_at: :desc) }
   validates :article_id, presence: true
+  validates :user_id, presence: true
   validates :text, presence: true, length: { maximum: 150 }
   validates :text, presence: true, length: { maximum: 150 }
 end
--- db/migrate/20200812122207_add_user_id_to_comment.rb
+++ db/migrate/20200812122207_add_user_id_to_comment.rb
@@ -1,6 +1,5 @@
 class AddUserIdToComment < ActiveRecord::Migration[6.0]
   def change
-    add_column :comments, :user_id, :integer, {null: false, default: 1}
-
+    add_reference :comments, :user, foreign_key: true
   end
 end
