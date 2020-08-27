6d2f4aa0de67d7c12af096031950454151adb4f5
重田 将人 <masato.0928.j@gmail.com>
2020-08-27 19:00:57 +0900

    いいね作成ajax化

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,19 +1,12 @@
 class GoodsController < ApplicationController
   before_action :sign_in?, only: [:create, :destroy]
+  before_action :generate_instance, only: [:create, :destroy]
 
   def create
-    if request.referrer.include?('articles/')
-      # コメントフォーム生成用
-      @comment = Comment.new
-    else
-      @articles = Article.all
-    end
-
-    @article = Article.includes(comments: :user).find_by(id: params[:article_id])
-
     current_user.do_thumb_up(good_param)
+
     respond_to do |format|
-      format.html { redirect_to redirect_to request.referrer }
+      format.html { redirect_to request.referrer || root_path }
       format.js
     end
   end
@@ -27,5 +20,15 @@ class GoodsController < ApplicationController
 
   def good_param
     params.required(:article_id)
+  end
+
+  def generate_instance
+    if request.referer.include?('articles/')
+      # コメントフォーム生成用
+      @comment = Comment.new
+    else
+      @articles = Article.includes(comments: :user).all
+    end
+    @article = (@articles || Article.includes(comments: :user)).find_by(id: params[:article_id])
   end
 end
