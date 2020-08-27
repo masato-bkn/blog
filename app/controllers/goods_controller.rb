c3c734a94f8e8edcd8697712b08a8d6cdffddc06
重田 将人 <masato.0928.j@gmail.com>
2020-08-27 20:27:16 +0900

    いいね削除のajax化

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,8 +1,8 @@
 class GoodsController < ApplicationController
   before_action :sign_in?, only: [:create, :destroy]
-  before_action :generate_instance, only: [:create, :destroy]
 
   def create
+    generate_instance(params[:article_id])
     current_user.do_thumb_up(good_param)
 
     respond_to do |format|
@@ -12,8 +12,15 @@ class GoodsController < ApplicationController
   end
 
   def destroy
+    if current_user.goods.find_by(id: params[:id]).present?
+      generate_instance(current_user.goods.find_by(id: params[:id]).article_id)
+    end
     current_user.do_thumb_down(params[:id])
-    redirect_to root_path
+
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_path }
+      format.js
+    end
   end
 
   private
@@ -22,13 +29,13 @@ class GoodsController < ApplicationController
     params.required(:article_id)
   end
 
-  def generate_instance
-    if request.referer.include?('articles/')
+  def generate_instance(id)
+    if request.referer&.include?('articles/')
       # コメントフォーム生成用
       @comment = Comment.new
     else
       @articles = Article.includes(comments: :user).all
     end
-    @article = (@articles || Article.includes(comments: :user)).find_by(id: params[:article_id])
+    @article = (@articles || Article.includes(comments: :user)).find_by(id: id)
   end
 end
