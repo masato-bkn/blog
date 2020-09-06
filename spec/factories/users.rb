803518bc0a133226c5bf0df8ae287ff8fbce8506
重田 将人 <masato.0928.j@gmail.com>
2020-09-06 18:52:52 +0900

    factories/usersから非必須項目を削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,17 +2,13 @@ FactoryBot.define do
   factory :user1, class: User do
     id { 1 }
     name { 'test' }
-    email { 'test1@gmail.com' }
+    email { "test1@gmail.com" }
     password { '1234ABC' }
-    updated_at { '2020-07-27 22:52:09' }
-    created_at { '2020-07-27 22:52:09' }
   end
   factory :user2, class: User do
     id { 2 }
     name { 'test2' }
     email { 'test2@gmail.com' }
     password { '1234ABC2' }
-    updated_at { '2020-07-28 22:52:09' }
-    created_at { '2020-07-28 22:52:09' }
   end
 end
