7af1fbc6a68d29ee6ab4a419c9e37c81f3fcbc1c
重田 将人 <masato.0928.j@gmail.com>
2020-09-07 09:27:29 +0900

    factories/article_goodsから不要な項目を削除(article,userはcreateするときに渡す)

--- spec/factories/article_goods.rb
+++ spec/factories/article_goods.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :article_good1, class: ArticleGood do
     id { 1 }
-    user_id { 1 }
-    article_id { 1 }
-    updated_at { '2020-07-27 22:52:09' }
-    created_at { '2020-07-27 22:52:09' }
   end
 end
