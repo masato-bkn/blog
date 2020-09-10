1f3bf3a079867c18a793517551c074b6f5e8371b
重田 将人 <masato.0928.j@gmail.com>
2020-09-10 10:25:05 +0900

    articles_goodsコントローラのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Rails.application.routes.draw do
   root 'home#top'
   resources :users
   resources :articles do
-    resources :goods
+    resources :goods, only: [:create, :destroy], controller: 'articles/goods'
   end
-  # TODO リファクタ後に削除する
-  resources :article_goods, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
   resources :comment_goods, only: [:create, :destroy]
 end
