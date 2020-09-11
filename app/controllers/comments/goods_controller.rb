093e9b1a8bc9ca69ce9fc7ac5d142833664b8d9f
重田 将人 <masato.0928.j@gmail.com>
2020-09-11 21:38:44 +0900

    comment_good_controllerのnamespaceを変更

---
+++ app/controllers/comments/goods_controller.rb
@@ -0,0 +1,31 @@
+class Comments::GoodsController < ApplicationController
+  before_action :sign_in?, only: [:create, :destroy]
+
+  def create
+    if @comment = Comment.find_by(id: params[:comment_id])
+      @comment.goods.create(user_id: current_user.id)
+
+      # 最新のgood_countを@commentに反映させるため
+      @comment.reload
+    end
+
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_path }
+      format.js { render 'comments/goods/destroy.js.erb' }
+    end
+  end
+
+  def destroy
+    if @comment = Comment.find_by(id: params[:comment_id])
+      @comment.goods.find_by(user_id: current_user.id)&.destroy
+
+      # 最新のgood_countを@commentに反映させるため
+      @comment.reload
+    end
+
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_path }
+      format.js { render 'comments/goods/create.js.erb' }
+    end
+  end
+end
